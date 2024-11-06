1b07725c35c0feb982c558eb58cef1d868a433a1
taha-ishaq <tahaishaq7346@gmail.com>
2024-11-07 00:04:39 +0500

    ling

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -61,7 +61,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[200px] lg:w-[300px] lg:h-[300px] relative">
             <Image
               src="/images/hero-image.png"
               alt="hero image"
